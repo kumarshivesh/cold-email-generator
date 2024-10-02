ecc2a469c98878102a0fb8033a8c86569ed747f8
Kumar Shivesh <116376181+kumarshivesh@users.noreply.github.com>
2024-10-02 17:43:20 +0530

    Add all initial files

---
+++ chains.py
@@ -0,0 +1,60 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Kumar Shivesh, a Python backend developer with hands-on experience in generative AI and DevOps. 
+            Your tech stack includes Python, Django, and PostgreSQL, and you currently work as a Python backend developer 
+            at a Fintech startup. Your role involves designing and developing scalable backend systems and integrating cutting-edge AI solutions.
+    
+            Write a cold email to the client regarding the job mentioned above, explaining how your expertise and experience can fulfill 
+            their needs. Highlight your relevant skills in Python, Django, PostgreSQL, and any relevant AI or DevOps capabilities. 
+            Also, incorporate the most relevant ones from the following links to showcase your portfolio: {link_list}
+            The email should convey confidence in your ability to deliver quality results.
+    
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
